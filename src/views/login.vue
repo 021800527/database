92086b399587382526145f986f503c66f706e052
021800527 <978621025@qq.com>
2020-11-15 01:11:47 +0800

    差其他方式登录的图片

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,6 +3,13 @@
     <img id="background_img" src="../assets/background.jpg">
     <div id="login_view">
       <img id="main_img" src="../assets/main_img.png">
+      <div id="login_img">
+      <img src="../assets/login_img.png">
+      <div id="login_title">登录</div>
+      <input id="user" placeholder="学号" type="text">
+      <input id="password" placeholder="密码" type="text">
+      <input id="login" type="button" value="立即登录">
+      </div>
     </div>
   </div>
 </template>
