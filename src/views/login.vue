6d3393ce80081aab4cbe06efcaa4997f7c09de23
021800527 <978621025@qq.com>
2020-11-15 01:54:14 +0800

    首页简历完毕，明天做centos的xshell连接，然后域名解析，以及路由守卫的设置

--- src/views/login.vue
+++ src/views/login.vue
@@ -4,11 +4,18 @@
     <div id="login_view">
       <img id="main_img" src="../assets/main_img.png">
       <div id="login_img">
-      <img src="../assets/login_img.png">
+      <img src="../assets/login_img.png" id="login_img_img">
       <div id="login_title">登录</div>
       <input id="user" placeholder="学号" type="text">
       <input id="password" placeholder="密码" type="text">
       <input id="login" type="button" value="立即登录">
+      <div id="no_count">没有账号？立即注册</div>
+      <div id="other_count">使用第三方登录</div>
+      <div id="other_counts">
+        <img src="../assets/webo.png" id="weibo_img">
+        <img src="../assets/wechat.png" id="wechat_img">
+        <img src="../assets/qq.png" id="qq_img">
+      </div>
       </div>
     </div>
   </div>
