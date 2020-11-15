297eda51141fa17f0573d7709678e6783a96064a
021800527 <978621025@qq.com>
2020-11-15 20:26:27 +0800

    现在我已经搞定了centos中python3.6，暂时先加入datav的依赖，我要做数据可视化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import dataV from '@jiaminghi/data-view'
 
+Vue.use(dataV)
 Vue.config.productionTip = false
 
 new Vue({
