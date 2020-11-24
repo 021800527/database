4cf9970416fbb29b7ded6f117e577ac332e7fcd2
021800527 <978621025@qq.com>
2020-11-24 19:56:46 +0800

    增加侧边栏的伸缩，图标待定，现在开始设计表

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="max_box">
-    <div id="left" v-on:mouseenter="check_enter" @mouseleave="check_leave" v-bind:style="{'-webkit-animation':enter?'move 1s 1 forwards':'no_move 1s 1'}"></div>
+    <img src="../assets/background.jpg" id="background_img">
+    <div id="up" v-bind:style="{'-webkit-animation':enter?'title_move 400ms 1 forwards':'title_no_move 400ms 1'}" >千金难买我开心</div>
+    <div id="left" ref="check_width" v-on:mouseenter="check_enter" @mouseleave="check_leave" v-bind:style="{'-webkit-animation':enter?'move 400ms 1 forwards':'no_move 400ms 1'}">
+      <div class="logo" v-bind:class="{active:enter}">
+        <p id="title" v-show="enter">福州大学选修课系统</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,7 +19,8 @@ export default {
   name:'Index',
   data:function (){
     return{
-      enter:false
+      enter:false,
+      width:0
     }
   },
   methods:{
@@ -22,7 +29,11 @@ export default {
     },
     check_enter:function (){
       this.enter  = true
+    },
+    check_width :function (){
+      this.width = this.$refs.check_width.offsetWidth
+      return this.width
     }
   }
-  }
+}
 </script>
